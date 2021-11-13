d96fa8e73ac27db774eedb0cf558f822cc7404f3
Denis Chmel <denis.chmel@gmail.com>
2021-11-12 23:11:20 -0500

    For GPL-5235 mongodb for Amadeus MPIS Cache

--- up.sh
+++ up.sh
@@ -1,1 +1,2 @@
-docker-compose up -d nginx mariadb redis workspace elasticsearch php-worker php-fpm
+docker-compose up -d nginx mariadb redis workspace elasticsearch php-worker php-fpm mongo
+docker exec -it -u laradock gvv-workspace-1 bash
