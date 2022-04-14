aa2a42e7213b00f1f72b458e7fc5439a33cfc4b4
Andrew Nedoluzhko <scarium0706@gmail.com>
2022-04-14 11:18:30 +0300

    Fix docker-compose up issue "Circular dependency between elasticsearch and docker-in-docker and php-fpm and workspace"

--- up.sh
+++ up.sh
@@ -1,2 +1,2 @@
 docker-compose up -d nginx mariadb redis workspace elasticsearch php-worker php-fpm mongo
-docker exec -it -u laradock gvv-workspace-1 bash
+docker exec -it -u laradock gvv_workspace_1 bash
