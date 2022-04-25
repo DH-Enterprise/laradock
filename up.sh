ca2862a319ac5be8cb416bcb6c76ae0b207e87b7
Denis Chmel <denis.chmel@gmail.com>
2022-04-25 16:06:25 -0400

    up.sh - More universal way to enter workspace container

--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
 docker-compose up -d nginx mariadb redis workspace elasticsearch php-worker php-fpm mongo
-docker exec -it -u laradock gvv_workspace_1 bash
+#docker exec -it -u laradock gvv_workspace_1 bash
+docker-compose exec workspace bash
